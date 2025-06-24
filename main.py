2ad62d2b37fb9bbfc2237e53a2d076c4b7553b25
Manish01399 <manish0139kumar@gmail.com>
2025-06-24 17:00:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -63,8 +63,8 @@ BUTTONSCONTACT = InlineKeyboardMarkup([[InlineKeyboardButton(text="📞 Contact"
 keyboard = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton(text="🛠️ Help", url="https://t.me/+3k-1zcJxINYwNGZl"),
-            InlineKeyboardButton(text="🛠️ Repo", url="https://github.com/nikhilsainiop/saini-txt-direct"),
+            InlineKeyboardButton(text="🛠️ Channel", url="https://t.me/+sjGGT5ughqYxYmVl"),
+            InlineKeyboardButton(text="🛠️ Contact", url="Manish_0139"),
         ],
     ]
 )
