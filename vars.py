efbbd82f56406fa65438de91dbff05929ae7e888
sakkskdnsnsms <sainathsandy123@gmail.com>
2025-06-26 15:32:16 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -6,7 +6,7 @@ from os import environ
 API_ID = int(environ.get("API_ID", "xxxxxxxxx"))
 API_HASH = environ.get("API_HASH", "xxxxxxxxxxxxxxxxxxxxxxx")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(environ.get("OWNER", "7939644457"))
+OWNER = int(environ.get("OWNER", "853356848"))
 CREDIT = "🅼🅰🅽🅸🆂🅷 🅺🆄🅼🅰🆁"
 AUTH_USER = os.environ.get('AUTH_USERS', '853356848').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
